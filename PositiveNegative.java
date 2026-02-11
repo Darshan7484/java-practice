395330a911bd42fc95f4fd9bbd2fa9409511803d
Darshan7484 <darshandarahu044@gmail.com>
2026-02-11 10:42:04 +0530

    Updated logic

---
+++ PositiveNegative.java
@@ -0,0 +1,14 @@
+public class PositiveNegative {
+    public static void main(String[] args) {
+
+        int number = -25;
+
+        if (number > 0) {
+            System.out.println(number + " is a Positive number");
+        } else if (number < 0) {
+            System.out.println(number + " is a Negative number");
+        } else {
+            System.out.println("The number is Zero");
+        }
+    }
+}
